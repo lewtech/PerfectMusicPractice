42539c9d130a5559b26b6f2b38a7975c845daf23
Lew Flauta <lewtech@gmail.com>
2015-06-03 20:35:22 -0500

    Initial Commit

---
+++ PerfectPianoPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PerfectPianoPractice
+//
+//  Created by lew flauta on 6/3/15.
+//  Copyright (c) 2015 lew flauta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
